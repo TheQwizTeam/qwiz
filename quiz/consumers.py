c920fd4bec434ac36527c25029c1bebc3b566767
Christopher Kane <ckane386@gmail.com>
2017-05-04 14:03:19 +0100

    Creating room on connect, checking for type on receive

--- quiz/consumers.py
+++ quiz/consumers.py
@@ -8,33 +8,36 @@ log = logging.getLogger(__name__)
 
 @channel_session
 def ws_connect(message):
-    # Extract the room fr:wqom the message. This expects message.path to be of the
-    # form /quiz/{room}/{handle}/
+    # Extract the room from the message. This expects message.path to be of the
+    # form /quiz/{name}/{handle}/
     # A Room will be retrieved, and created if it does not already exits.
     # If the path is invalid, we bail (meaning this is a some othersort
     # of websocket). 
     try:
-        prefix, room, handle = message['path'].decode('ascii').strip('/').split('/')
+        prefix, room_name, handle = message['path'].decode('ascii').strip('/').split('/')
         if prefix != 'quiz':
             log.debug('invalid ws path=%s', message['path'])
             return
     except ValueError:
         log.debug('invalid ws path=%s', message['path'])
         return
-    log.debug('quiz monster connect room=%s handle=%s client=%s:%s', 
-        room, handle, message['client'][0], message['client'][1])
+
+    room, created = Room.objects.get_or_create(name=room_name)
+
+    log.debug('quiz monster connect room_name=%s handle=%s client=%s:%s', 
+        roon.name, handle, message['client'][0], message['client'][1])
     
     # Need to be explicit about the channel layer so that testability works
     # This may be a FIXME?
-    Group('quiz-'+room, channel_layer=message.channel_layer).add(message.reply_channel)
+    Group('quiz-'+room.name, channel_layer=message.channel_layer).add(message.reply_channel)
 
-    message.channel_session['room'] = room
+    message.channel_session['room_name'] = room.name
 
 @channel_session
 def ws_receive(message):
     # Look up the room from the channel session, bailing if it doesn't exist
     try:
-        room = message.channel_session['room']
+        room_name = message.channel_session['room_name']
     except KeyError:
         log.debug('no room in channel_session')
         return
@@ -47,13 +50,13 @@ def ws_receive(message):
         log.debug("ws message isn't json text=%s", text)
         return
     
-    if set(data.keys()) != set(('handle', 'message')):
+    if set(data.keys()) != set(('type', 'handle', 'message')):
         log.debug("ws message unexpected format data=%s", data)
         return
 
     if data:
-        log.debug('chat message room=%s handle=%s message=%s', 
-            room, data['handle'], data['message'])
+        log.debug('chat message room_name=%s type=%s handle=%s message=%s', 
+            room_name, data['type'], data['handle'], data['message'])
 
         # See above for the note about Group
-        Group('quiz-'+room, channel_layer=message.channel_layer).send({'text': json.dumps(data)})
+        Group('quiz-'+room_name, channel_layer=message.channel_layer).send({'text': json.dumps(data)})
