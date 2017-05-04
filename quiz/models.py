9d573799fb6dad652a659424c85ba2794b99687c
Christopher Kane <ckane386@gmail.com>
2017-05-04 12:54:54 +0100

    First stab at our models

---
+++ quiz/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+
+class Question(models.Model):
+    # Room
+    question = models.ForeignKey(Room, on_delete=models.CASCADE)
+    # Question
+    question_text = models.CharField(max_length=200)
+    # Answers
+    correct_answer = models.CharField(max_length=200)
+    incorrect_answer_1 = models.CharField(max_length=200)
+    incorrect_answer_2 = models.CharField(max_length=200)
+    incorrect_answer_3 = models.CharField(max_length=200)
+
+class Contestant(models.Model):
+    handle = models.CharField(max_length=200)
+    score = models.IntegerField(default=0)
