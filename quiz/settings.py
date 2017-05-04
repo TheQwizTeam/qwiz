b7615062a9ae7a1f1ba030870ee5a4520843c8c2
Christopher Kane <ckane386@gmail.com>
2017-05-04 09:56:39 +0100

    Magic fix

--- quiz/settings.py
+++ quiz/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'channels',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
