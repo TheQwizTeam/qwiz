44211c83c7b567d04c781c395a98e9a5588e1714
Christopher Kane <ckane386@gmail.com>
2017-05-04 14:38:45 +0100

    adding __str__ methods

--- quiz/models.py
+++ quiz/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Question(models.Model):
     # Room
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
@@ -14,6 +17,9 @@ class Question(models.Model):
     incorrect_answer_2 = models.CharField(max_length=200)
     incorrect_answer_3 = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.question_text
+
 class Contestant(models.Model):
     # Room
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
@@ -21,3 +27,6 @@ class Contestant(models.Model):
     handle = models.CharField(max_length=200)
     # user's score
     score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.handle
