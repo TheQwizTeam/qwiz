e9bbc9a1f2cef103d24220010745bc4c3cc54aab
Paul Crooks <pgcrooks@gmail.com>
2017-11-02 16:15:50 +0000

    Adding more tests

--- quiz/tests.py
+++ quiz/tests.py
@@ -3,14 +3,52 @@ import datetime
 from django.utils import timezone
 from django.test import TestCase
 
-from .models import Room
+from .models import Room, QuestionVault, Contestant
 
 
-class RoomModelTests(TestCase):
+class QuizModelTests(TestCase):
+
+    def test_new_room_no_name(self):
+        """
+        Add a new room with no name.
+        """
+        r = Room()
+        with self.assertRaises(TypeError):
+            r.save()
 
     def test_new_room(self):
         """
         Add a new room.
         """
-        room = Room(name='test_room')
-        self.assertEqual(room.__str__(), 'test_room')
+        r = Room(name='test_room')
+        self.assertEqual(r.__str__(), 'test_room')
+
+    def test_new_question_vault_no_str(self):
+        """
+        Add a new question with no text.
+        """
+        q = QuestionVault()
+        with self.assertRaises(TypeError):
+            q.save()
+
+    def test_new_question_vault(self):
+        """
+        Add a new question.
+        """
+        q = QuestionVault(question_text='foo')
+        self.assertEqual(q.__str__(), 'foo')
+
+    def test_new_contestant_no_name(self):
+        """
+        Add a new Contestant with no name.
+        """
+        c = Contestant()
+        with self.assertRaises(TypeError):
+            c.save()
+
+    def test_new_contestant(self):
+        """
+        Add a new Contestant.
+        """
+        c = Contestant(handle='bob')
+        self.assertEqual(c.__str__(), 'bob')
