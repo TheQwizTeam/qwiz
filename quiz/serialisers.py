ca1baa3a0100406d3727a8e90e9663e649f1f481
Christopher Kane <ckane386@gmail.com>
2017-12-21 13:49:11 +0000

    Adding supplementary data to Room Create API
    Room create now accepts a list of tags, as well as the number of
    questions. Additionally, the response (and related GET request) returns
    room name and code, hiding other information

--- quiz/serialisers.py
+++ quiz/serialisers.py
@@ -12,17 +12,33 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
         model = Tag
         fields = '__all__'
 
+
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
     """
     Room object serializer
     """
+    tags = serializers.ListField(
+        write_only=True,
+        child=serializers.CharField(write_only=True),
+    )
+    num_questions = serializers.IntegerField(max_value=10, min_value=1, write_only=True)
+
+    def create(self, validated_data):
+        # Remove supplementary data, which will be used to populate the room
+        # model with questions once it has been instantiated.
+        tags = validated_data.pop('tags')
+        num_questions = validated_data.pop('num_questions')
+
+        return super(RoomSerializer, self).create(validated_data)
+
     class Meta:
         """
         Serialize all fields in the Room object
         """
         model = Room
-        read_only_fields = ('code', 'questions')
-        fields = '__all__'
+        fields = ('name', 'code', 'tags', 'num_questions')
+        read_only_fields = ('code',)
+
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     """
--- quiz/views.py
+++ quiz/views.py
@@ -4,12 +4,14 @@ from rest_framework import viewsets
 from quiz.models import Tag, Room, Question
 from quiz.serialisers import TagSerializer, RoomSerializer, QuestionSerializer
 
+
 def index(request):
     """
     Render index.html
     """
     template = loader.get_template("index.html")
     return HttpResponse(template.render())
+
 
 class TagViewSet(viewsets.ModelViewSet):
     """
@@ -18,12 +20,14 @@ class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
+
 class RoomViewSet(viewsets.ModelViewSet):
     """
     API Endpoint that allows Rooms to be viewed
     """
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
 
 class QuestionViewSet(viewsets.ModelViewSet):
     """
