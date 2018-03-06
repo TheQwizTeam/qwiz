7bad533babfe47458f4d9bf0da82b6babe39679d
Christopher Kane <ckane386@gmail.com>
2018-03-06 22:57:51 +0000

    Added further WebSocket tests
    Added additional test to verify "Contestant List" server to client
    communication

--- quiz/test_websocket.py
+++ quiz/test_websocket.py
@@ -66,3 +66,82 @@ class WebSocketCreateContestantTests(ChannelTestCase):
         self.assertEquals(room.contestant_set.last().handle, 'Joe Bloggs')
 
         client.send_and_consume('websocket.disconnect')
+
+
+class WebSocketContestantListTests(ChannelTestCase):
+    """
+    WebSocket contestant list WebSocket API tests
+    """
+    def setUp(self):
+        """
+        Room test setup, creating a single room
+        """
+        room = Room(name='Test Room')
+        room.save()
+
+    def tearDown(self):
+        """
+        Room test teardown, removing all created rooms
+        """
+        Room.objects.all().delete()
+
+    def test_create_contestant_invalid_room(self):
+        """
+        Attempt to create multiple contestants, and verify the contestant
+        list propagated to all contestants is correct
+        """
+        john_client = WSClient()
+        jane_client = WSClient()
+
+        john_client.send_and_consume('websocket.connect')
+        john_client.send_and_consume('quiz.receive',{
+            'command': 'new_contestant',
+            'room_code': Room.objects.last().code,
+            'contestant_name': 'John Doe'
+        })
+
+        # Verify server responded with correct status code
+        response = john_client.receive()
+        self.assertEqual(response['status_code'], StatusCode.OK)
+        # Verify server updates john with complete contestant list
+        response = john_client.receive()
+        self.assertDictEqual(response, {
+            'command': 'contestant_list',
+            'contestants': [
+                'John Doe'
+            ]
+        })
+
+        jane_client.send_and_consume('websocket.connect')
+        jane_client.send_and_consume('quiz.receive',{
+            'command': 'new_contestant',
+            'room_code': Room.objects.last().code,
+            'contestant_name': 'Jane Doe'
+        })
+
+        # Verify server responded with correct status code
+        response = jane_client.receive()
+        self.assertEqual(response['status_code'], StatusCode.OK)
+        # Verify server updates jane with complete contestant list
+        response = jane_client.receive()
+        self.assertDictEqual(response, {
+            'command': 'contestant_list',
+            'contestants': [
+                'Jane'
+                ' Doe',
+                'John Doe'
+            ]
+        })
+
+        # Verify server updates john with complete contestant list
+        response = john_client.receive()
+        self.assertDictEqual(response, {
+            'command': 'contestant_list',
+            'contestants': [
+                'Jane Doe',
+                'John Doe'
+            ]
+        })
+
+        jane_client.send_and_consume('websocket.disconnect')
+        john_client.send_and_consume('websocket.disconnect')
