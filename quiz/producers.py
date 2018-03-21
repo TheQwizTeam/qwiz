7dc8de6855331658faab7c9829ded76ce257f54d
Christopher Kane <ckane386@gmail.com>
2018-03-21 00:55:14 +0000

    Added delayable responses

--- quiz/producers.py
+++ quiz/producers.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+from celery import task
 import json
 from enum import IntEnum, unique
 from channels import Channel
@@ -10,7 +12,7 @@ class StatusCode(IntEnum):
     ILLEGAL_ARGUMENT = 2
 
 
-def send_response(channel, status_code=StatusCode.OK, message=None):
+def send_response(channel, status_code=StatusCode.OK, message=None, delay=None):
     if not isinstance(status_code, StatusCode):
         raise TypeError('Inappropriate status code')
 
@@ -21,4 +23,12 @@ def send_response(channel, status_code=StatusCode.OK, message=None):
     if message:
         response['message'] = message
 
+    if delay:
+        channel_send.apply_async(args=(channel, response), countdown=delay)
+    else:
+        channel_send(channel, response)
+
+
+@task
+def channel_send(channel, response):
     Channel(channel).send({'text': json.dumps(response)})
