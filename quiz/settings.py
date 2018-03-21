f37396a925291946440ccf52fbd443268a62e349
Christopher Kane <ckane386@gmail.com>
2018-03-21 00:28:50 +0000

    Enabling producers module with celery task capabilities

--- quiz/settings.py
+++ quiz/settings.py
@@ -160,3 +160,4 @@ CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+CELERY_IMPORTS = ['quiz.producers']
