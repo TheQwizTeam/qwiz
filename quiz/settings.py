a13e3749e31342a9c4910cef7566f6ff037a9f56
Christopher Kane <ckane386@gmail.com>
2018-03-21 00:17:10 +0000

    Removing tz (default UTC) and making broker/result environment indepdent

--- quiz/settings.py
+++ quiz/settings.py
@@ -155,9 +155,8 @@ FIXTURE_DIRS = (
 )
 
 # CELERY STUFF
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Europe/London'
